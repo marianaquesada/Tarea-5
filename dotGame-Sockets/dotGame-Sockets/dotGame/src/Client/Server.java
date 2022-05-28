15d0450adece51c2e3b116898ce29e0302929d67
marianaquesada <99993176+marianaquesada@users.noreply.github.com>
2022-05-27 22:31:03 -0600

    Add files via upload

---
+++ dotGame-Sockets/dotGame-Sockets/dotGame/src/Client/Server.java
@@ -0,0 +1,39 @@
+package Client;
+
+import java.net.*;
+import java.io.*;
+import Common.*;
+
+public class Server implements Runnable{
+    ServerSocket server;
+    Socket client;
+    ObjectInputStream input;
+    Dot dot;
+
+    public void Client(Dot d){
+        dot = d;
+        try {
+            server = new ServerSocket(4445);
+        } 
+        catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+    }
+
+    public void run(){
+        try {
+            while(true){
+                client = server.accept();
+                input = new ObjectInputStream(client.getInputStream());
+                dot.target = (Target)input.readObject();
+                input.close();
+                client.close();
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+    }
+    
+}
